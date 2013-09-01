571076d18ccf817fcb71f7c1b27d61b0b4d02fb5
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2013-09-01 16:57:55 +0400

    more CPU-intensive benchmark, add profiling utils

--- trnado.py
+++ trnado.py
@@ -4,18 +4,39 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
+from utils import debug_exec, is_prime
+
+
 class ServeHTTP(tornado.web.RequestHandler):
     def get(self, txt, num):
         for i in xrange(int(num)):
             self.write("%d: %s\n" % (i, txt))
 
 
+class ServeHTTPPrimes(tornado.web.RequestHandler):
+    def get(self, txt, num):
+        for i in xrange(int(num)):
+            if is_prime(i):
+                self.write("%d: %s\n" % (i, txt))
+
+
+class SimpleServeHTTP(tornado.web.RequestHandler):
+    def get(self):
+        self.write("hi!")
+
+
 urls = [
-    ("^/([^/]+)/([0-9]+)$", ServeHTTP)
+    ("^/([^/]+)/([0-9]+)$", ServeHTTPPrimes)
+    #("^/([^/]+)/([0-9]+)$", ServeHTTP)
+    #("^/$", SimpleServeHTTP)
 ]
 app = tornado.web.Application(urls)
+
 
 if __name__ == "__main__":
     server = tornado.httpserver.HTTPServer(app)
     server.listen(int(sys.argv[1]))
-    tornado.ioloop.IOLoop.instance().start()
+    start = tornado.ioloop.IOLoop.instance().start
+    if '--profile' in sys.argv[2:]:
+        start = debug_exec(stat_profile=True)(start)
+    start()
---
+++ utils.py
@@ -0,0 +1,56 @@
+# -*- encoding: utf-8 -*-
+
+import logging; logger = logging.getLogger(name=__name__)
+from functools import wraps
+import time
+import traceback
+
+
+def debug_exec(*deco_args, **deco_kwargs):
+    ''' Output execution time to logger.debug, additionally:
+    profile = True  - profile with cProfile,
+    stat_profile = True - profile with statprof,
+    traceback = True - print traceback before each call
+    '''
+    def deco(fn):
+        @wraps(fn)
+        def inner(*args, **kwargs):
+            log_fn = logger.debug
+            if deco_kwargs.get('traceback'):
+                traceback.print_stack()
+            if deco_kwargs.get('traceback'):
+                traceback.print_stack()
+            log_fn('starting %s', fn.__name__)
+            start = time.time()
+            stat_profile = deco_kwargs.get('stat_profile')
+            if stat_profile:
+                import statprof
+                statprof.reset(frequency=10000)
+                statprof.start()
+            try:
+                return fn(*args, **kwargs)
+            finally:
+                log_fn('finished %s in %s s', fn.__name__, time.time() - start)
+                if stat_profile:
+                    statprof.stop()
+                    statprof.display()
+        if deco_kwargs.get('profile'):
+            import profilehooks
+            inner = profilehooks.profile(immediate=True)(inner)
+        return inner
+    if deco_args:
+        return deco(deco_args[0])
+    else:
+        return deco
+
+
+def is_prime(n):
+    for i in xrange(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+assert is_prime(7)
+assert is_prime(31)
+assert not is_prime(7 * 31)
